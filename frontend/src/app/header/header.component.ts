41b4468f82414d73d4aae9bdba226e30ee6cc210
kaidux22 <kaidux22@gmail.com>
2024-12-08 18:13:29 +0300

    Add search icon

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
 export class HeaderComponent {
   is_curtain_page = true;
-  is_logged_in = true;
+  is_logged_in = false;
   is_admin = true;
   fullname = "Прошичев Александр"
 
   constructor(private router: Router) {}
 
   navigateToLogin() {
-    this.router.navigate(['/login']);
+    //this.router.navigate(['/login']);
+    this.is_logged_in = !this.is_logged_in;
+
   }
 }
