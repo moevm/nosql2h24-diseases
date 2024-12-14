5fd0d5afcd632dc9a7f5e8c9bda5aee92a63976e
kaidux22 <kaidux22@gmail.com>
2024-12-14 03:37:10 +0300

    filtering

--- frontend/src/app/dbases/dbases.component.ts
+++ frontend/src/app/dbases/dbases.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dbases',
@@ -17,6 +17,9 @@ export class DbasesComponent {
   data: any = null;
   items: any = [];
   idx: number = 0;
+
+  page: number = 1;
+  currect_enters: any = [];
 
   disease_filter: any = {
     name: '',
@@ -55,6 +58,9 @@ export class DbasesComponent {
 
   GoToDB(type: string){
     this.type = type
+    this.currect_enters = []
+    this.items = []
+    this.page = 1
     this.MakePostReq(this.type)
   }
 
@@ -82,6 +88,13 @@ export class DbasesComponent {
         this.data['filter_params'][`filter${this.idx}-field`] = 'disease_recommendations'
         this.data['filter_params'][`filter${this.idx}-action`] = 'CONTAINS'
         this.data['filter_params'][`filter${this.idx}-value`] = "'" + this.disease_filter['recommendation'] + "'"
+        this.idx += 1
+      }
+
+      if(this.disease_filter['type']){
+        this.data['filter_params'][`filter${this.idx}-field`] = 'disease_type'
+        this.data['filter_params'][`filter${this.idx}-action`] = 'CONTAINS'
+        this.data['filter_params'][`filter${this.idx}-value`] = "'" + this.disease_filter['type']  + "'"
         this.idx += 1
       }
 
@@ -145,8 +158,6 @@ export class DbasesComponent {
         this.idx += 1
       }
 
-      /*
-
       this.data['filter_params'][`filter${this.idx}-field`] = 'height'
       this.data['filter_params'][`filter${this.idx}-action`] = '>='
       this.data['filter_params'][`filter${this.idx}-value`] = `${this.patient_filter['from_height']}`
@@ -163,21 +174,60 @@ export class DbasesComponent {
       this.idx += 1
 
       this.data['filter_params'][`filter${this.idx}-field`] = 'weight'
-      this.data['filter_params'][`filter${this.idx}-action`] = '>='
+      this.data['filter_params'][`filter${this.idx}-action`] = '<='
       this.data['filter_params'][`filter${this.idx}-value`] = `${this.patient_filter['to_weight']}`
       this.idx += 1
 
-      */
     }
     else if(type == 'sympts'){
-      console.log(this.sympts_filter)
+      this.data = {"entity_type": "Symptom", "filter_params": {}}
+      this.idx = 1
+
+      if(this.sympts_filter['name']){
+        this.data['filter_params'][`filter${this.idx}-field`] = 'symptom_name'
+        this.data['filter_params'][`filter${this.idx}-action`] = 'CONTAINS'
+        this.data['filter_params'][`filter${this.idx}-value`] = "'" + this.sympts_filter['name'] + "'"
+        this.idx += 1
+      }
+      
+      if(this.sympts_filter['description']){
+        this.data['filter_params'][`filter${this.idx}-field`] = 'symptom_description'
+        this.data['filter_params'][`filter${this.idx}-action`] = 'CONTAINS'
+        this.data['filter_params'][`filter${this.idx}-value`] = "'" + this.sympts_filter['description'] + "'"
+        this.idx += 1
+      }
+
     } else{
-      console.log(this.appeal_filter)
+      this.data = {"entity_type": "Appeal", "filter_params": {}}
+      this.idx = 1
+
+      if(this.appeal_filter['from_appeal_datetime']){
+        this.data['filter_params'][`filter${this.idx}-field`] = 'appeal_date'
+        this.data['filter_params'][`filter${this.idx}-action`] = '>='
+        this.data['filter_params'][`filter${this.idx}-value`] = "'" + (this.appeal_filter['from_appeal_datetime'] ? this.appeal_filter['from_appeal_datetime'].split(' ')[0] : '1900-01-01') + "T00:00:00" + "'"
+        this.idx += 1
+      }
+
+      if(this.appeal_filter['to_appeal_datetime']){
+        this.data['filter_params'][`filter${this.idx}-field`] = 'appeal_date'
+        this.data['filter_params'][`filter${this.idx}-action`] = '<='
+        this.data['filter_params'][`filter${this.idx}-value`] = "'" + (this.appeal_filter['to_appeal_datetime'] ? this.appeal_filter['to_appeal_datetime'].split(' ')[0] : '2200-01-01') + "T23:59:59" + "'"
+        this.idx += 1
+      }
+
+      if(this.appeal_filter['complaints']){
+        this.data['filter_params'][`filter${this.idx}-field`] = 'appeal_complaints'
+        this.data['filter_params'][`filter${this.idx}-action`] = 'CONTAINS'
+        this.data['filter_params'][`filter${this.idx}-value`] = "'" + this.appeal_filter['complaints'] + "'"
+        this.idx += 1
+      }
     }
 
     this.http.post('http://127.0.0.1:5000/api/entities', this.data).subscribe({
       next: (response: any) => {
         this.items = response['ans']
+        this.currect_enters = this.items.slice(0, 10)
+        this.page = Math.min(1, this.totalPages)
         console.log(response['ans'])
       },
       error: error => {
@@ -187,6 +237,30 @@ export class DbasesComponent {
         console.log('Request complete');
       }
     });
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.items.length / 10);
+  }
+
+  FirstPage(){
+    this.page = 1
+    this.currect_enters = this.items.slice((this.page - 1) * 10, this.page * 10)
+  }
+
+  PreviousPage(){
+    this.page = Math.max(1, this.page - 1)
+    this.currect_enters = this.items.slice((this.page - 1) * 10, this.page * 10)
+  }
+
+  NextPage(){
+    this.page = Math.min(this.totalPages, this.page + 1)
+    this.currect_enters = this.items.slice((this.page - 1) * 10, this.page * 10)
+  }
+
+  LastPage(){
+    this.page = this.totalPages
+    this.currect_enters = this.items.slice((this.page - 1) * 10, this.page * 10)
   }
 
   ngOnInit(){
